1fbeb00b84aff2ad70c1fc9656a5b5f7511209db
kishoun <kellybrener66@gmail.com>
2024-11-27 15:32:48 +0100

    ex00_in_progress

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,24 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbrener- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 15:23:34 by kbrener-          #+#    #+#             */
+/*   Updated: 2024/11/27 15:29:04 by kbrener-         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include <algorithm>
+#include <list>
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+template<typename T>
+inputiterator	easyfind(T const & container, int n) {
+	std::find(T.begin(), T.end(), n);
+}
+
+#endif
