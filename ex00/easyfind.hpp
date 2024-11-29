1aa6b7dfbb950b4adabd0cb34dada602d03acf34
kishoun <kellybrener66@gmail.com>
2024-11-29 11:39:37 +0100

    ex00_done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,19 +6,39 @@
 /*   By: kbrener- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:23:34 by kbrener-          #+#    #+#             */
-/*   Updated: 2024/11/27 15:29:04 by kbrener-         ###   ########.fr       */
+/*   Updated: 2024/11/29 11:30:41 by kbrener-         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include <algorithm>
 #include <list>
+#include <deque>
+#include <vector>
+#include <iostream>
+#include <exception>
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+class	TargetNotFind : public std::exception {
+	private:
+	std::string	_message;
+	public:
+	TargetNotFind(std::string const & message) : _message(message) {}
+	virtual ~TargetNotFind() throw() {}
+	virtual const char* what() const throw() {
+		return (_message.c_str());
+	}
+};
+
 template<typename T>
-inputiterator	easyfind(T const & container, int n) {
-	std::find(T.begin(), T.end(), n);
+typename T::iterator	easyfind(T & container, int n) {
+	typename T::iterator	it;
+	it = std::find(container.begin(), container.end(), n);
+	if (it != container.end())
+		return it;
+	else
+		throw TargetNotFind("target not find in the container");
 }
 
 #endif
---
+++ ex00/main.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbrener- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 10:49:23 by kbrener-          #+#    #+#             */
+/*   Updated: 2024/11/29 11:31:11 by kbrener-         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include "easyfind.hpp"
+
+int main() {
+
+	int	int_tab[] = {51, 84512, 4 ,68, 7, 5, 4};
+	std::list<int>	int_list(int_tab, int_tab + sizeof(int_tab) / sizeof(int));
+	std::vector<int>	int_vector(int_tab, int_tab + sizeof(int_tab) / sizeof(int));
+	std::deque<int>	int_deque(int_tab, int_tab + sizeof(int_tab) / sizeof(int));
+
+	try {
+	std::cout<<*easyfind(int_list, 4)<<std::endl;
+	std::cout<<*easyfind(int_list, 2)<<std::endl;
+	} catch (std::exception & e) {
+		std::cout<<"ERROR : "<<e.what()<<std::endl;
+	}
+	try {
+	std::cout<<*easyfind(int_vector, 4)<<std::endl;
+	std::cout<<*easyfind(int_vector, 2)<<std::endl;
+	} catch (std::exception & e) {
+		std::cout<<"ERROR : "<<e.what()<<std::endl;
+	}
+	try {
+	std::cout<<*easyfind(int_deque, 4)<<std::endl;
+	std::cout<<*easyfind(int_deque, 2)<<std::endl;
+	} catch (std::exception & e) {
+		std::cout<<"ERROR : "<<e.what()<<std::endl;
+	}
+}
