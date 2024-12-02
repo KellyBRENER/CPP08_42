2ca2512ab67bc3dcdd81607418eadaf20d3b7f6d
kishoun <kellybrener66@gmail.com>
2024-12-02 15:06:09 +0100

    ex02_finish and check

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: kbrener- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 10:46:29 by kbrener-          #+#    #+#             */
-/*   Updated: 2024/12/02 12:05:56 by kbrener-         ###   ########.fr       */
+/*   Updated: 2024/12/02 14:50:17 by kbrener-         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -22,23 +22,26 @@
 
 //rendre std::stack itérable
 //dans la définition de la classe std::stack, le container est de type deque par défaut et stocké sous l'attribut privé "c"
-template< typename T = int>
-class MutantStack : public std::stack<T> {
+template< typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
 public:
-	MutantStack() {}
-	MutantStack(MutantStack const & src) : std::stack<T>(src) {}
+	MutantStack() : std::stack<T, Container>() {}
+	MutantStack(MutantStack const & src) : std::stack<T, Container>(src) {}
 	virtual ~MutantStack() {};
 	MutantStack &	operator=(MutantStack const & src) {
 		if (*this != src)
-			std::stack<T>::operator=(src);
+			std::stack<T, Container>::operator=(src);
 		return *this;
 	}
-	typedef typename std::stack<T>::container_type::iterator	iterator;//permet de définir une variable iterator du type de container utilisé
+	typedef typename std::stack<T, Container>::container_type::iterator	iterator;//permet de définir une variable iterator du type de container utilisé
 	iterator	begin() {
 		return(this->c.begin());
 	}
 	iterator	end() {
 		return (this->c.end());
+	}
+	void	push_back(T data) {
+		this->c.push_back(data);
 	}
 };
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kbrener- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 11:06:30 by kbrener-          #+#    #+#             */
-/*   Updated: 2024/12/02 11:06:45 by kbrener-         ###   ########.fr       */
+/*   Updated: 2024/12/02 15:04:12 by kbrener-         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,6 +14,7 @@
 
 int main()
 {
+	//tests from subject
 MutantStack<int> mstack;
 mstack.push(5);
 mstack.push(17);
@@ -35,6 +36,23 @@ std::cout << *it << std::endl;
 ++it;
 }
 std::stack<int> s(mstack);
+//My own test
+std::stack<int>	realStack;
+MutantStack<int, std::vector<int> >	mstack_vector;
+MutantStack<int, std::list<int> >	mstack_list;
+MutantStack<int, std::deque<int> >	mstack_deq;
+realStack.push(6);
+//realStack.push_back(10); not possible
+mstack_vector.push(6);
+mstack_list.push(6);
+mstack_deq.push(6);
+mstack_vector.push_back(10);
+mstack_list.push_back(10);
+mstack_deq.push_back(10);
+std::cout<<"top of mstack_deq: "<<mstack_deq.top()<<std::endl;
+std::cout<<"size of mstack_list: "<<mstack_list.size()<<std::endl;
+std::cout<<"first it of mstack_vector: "<<*mstack_vector.begin()<<std::endl;
+
 return 0;
 }
 
